2669a04140af392ea5c7afab939b45fe4c1f7127
marufei <867814102@qq.com>
2017-06-27 14:24:55 +0800

    2017.6.28 13:38

--- app/src/main/java/com/zys/jym/lanhu/activity/MyShare2Activity.java
+++ app/src/main/java/com/zys/jym/lanhu/activity/MyShare2Activity.java
@@ -216,6 +216,7 @@ public class MyShare2Activity extends BaseActivity implements View.OnClickListen
      */
     private void myShare(String platform) {
         MyUtils.Loge(TAG,"Regcode:"+getApplicationContext().getUser().getRegcode());
+        MyUtils.Loge(TAG,"url:"+getApplicationContext().getUser().getAppurl());
         if(!TextUtils.isEmpty(title)&&!TextUtils.isEmpty(content)&&getApplicationContext().getUser()!=null&&!TextUtils.isEmpty(getApplicationContext().getUser().getAppurl())&&!TextUtils.isEmpty(getApplicationContext().getUser().getRegcode())) {
             OnekeyShare oks = new OnekeyShare();
             if (platform != null)
@@ -225,21 +226,28 @@ public class MyShare2Activity extends BaseActivity implements View.OnClickListen
 
             // 分享时Notification的图标和文字  2.5.9以后的版本不     调用此方法
             //oks.setNotification(R.drawable.ic_launcher, getString(R.string.app_name));
+
             // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
             oks.setCallback(new MyCallBalk());
+
             oks.setTitle(title+getApplicationContext().getUser().getRegcode());
+
             // titleUrl是标题的网络链接，仅在人人网和QQ空间使用
             oks.setTitleUrl(getApplicationContext().getUser().getAppurl());
+
             // text是分享文本，所有平台都需要这个字段
             oks.setText(content);
+
             // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
             oks.setImagePath("");//确保SDcard下面存在此张图片
             // url仅在微信（包括好友和朋友圈）中使用
             oks.setUrl(getApplicationContext().getUser().getAppurl());
+            oks.setImageUrl(getApplicationContext().getUser().getAppurl());
             // comment是我对这条分享的评论，仅在人人网和QQ空间使用
 //            oks.setComment("我是测试评论文本");
             // site是分享此内容的网站名称，仅在QQ空间使用
             oks.setSite(getString(R.string.app_name));
+
             // siteUrl是分享此内容的网站地址，仅在QQ空间使用
             oks.setSiteUrl(getApplicationContext().getUser().getAppurl());
 
